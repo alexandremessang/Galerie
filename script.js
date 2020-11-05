269a78d8668dec2ce6452b4c9df41fcb7a910507
alexandremessang <alexandre.messang@gmail.com>
2020-11-05 09:51:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+var isLoad = false;
+
+function preset() {
+    document.getElementById("loading").style.visibility = "hidden";
+    loader();
+}
+
+function loader() {
+    if (!isLoad) {
+        document.getElementById("loading").style.visibility = "visible";
+        setTimeout(loadImages, 1000);
+    }
+}
+
+function loadImages() {
+    document.getElementById("loading").style.visibility = "hidden";
+    var imageBox = document.getElementById("image_box");
+    let numImage = 1;
+    const srcImages = [
+        'img/biking.jpg',
+        'img/kitesurf.jpg',
+        'img/ski.jpg',
+        'img/yoga.jpg'
+    ];
+
+    srcImages.forEach(src => {
+        let colDiv = document.createElement("div");
+        let cardDiv = document.createElement("div");
+        let img = document.createElement("img");
+        let cardBodyDiv = document.createElement("div");
+        let h5 = document.createElement("h5");
+        let p = document.createElement("p");
+        let a = document.createElement("a");
+
+        colDiv.className = "col col-sm-4";
+
+        cardDiv.className = "card";
+        cardDiv.style = "width: 18rem;";
+
+        img.src = src;
+        img.className = "card-img-top";
+
+        cardBodyDiv.className = "card-body";
+
+        h5.className = "card-title"
+        h5.textContent = "Titre " + numImage;
+
+        p.className = "card-text";
+        p.textContent = "Image " + numImage;
+
+        a.className = "btn btn-primary"
+        a.href = "#"
+        a.textContent = "En savoir plus"
+
+        colDiv.appendChild(cardDiv);
+        cardDiv.appendChild(img);
+        cardDiv.appendChild(cardBodyDiv);
+        cardBodyDiv.appendChild(h5);
+        cardBodyDiv.appendChild(p);
+        cardBodyDiv.appendChild(a);
+
+        imageBox.appendChild(colDiv);
+        numImage++;
+    });
+
+    isLoad = true;
+}
+    
+
+    
+
+    
