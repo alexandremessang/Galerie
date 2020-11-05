ecb8d25c6132282be23fa305a168889475489740
alexandremessang <alexandre.messang@gmail.com>
2020-11-05 15:16:57 +0100

    fetch7

--- script.js
+++ script.js
@@ -24,55 +24,59 @@ function loadImages() {
     ];
 
     window.fetch("./galerieRepos/images.json").then(function(res){
-        console.log(res);
         return res.json();
     }).then(function(response){
-        console.log(response);
+        const data = response;
+        
+        data.forEach(src => {
+            let colDiv = document.createElement("div");
+            let cardDiv = document.createElement("div");
+            let img = document.createElement("img");
+            let cardBodyDiv = document.createElement("div");
+            let h5 = document.createElement("h5");
+            let p = document.createElement("p");
+            let a = document.createElement("a");
+    
+            colDiv.className = "col col-sm-4";
+    
+            cardDiv.className = "card";
+            cardDiv.style = "width: 18rem;";
+    
+            img.src = src["url"];
+            img.className = "card-img-top";
+    
+            cardBodyDiv.className = "card-body";
+    
+            h5.className = "card-title"
+            h5.textContent = src["name"];
+    
+            p.className = "card-text";
+            p.textContent = src["description"];
+    
+            a.className = "btn btn-primary"
+            a.href = "#"
+            a.textContent = "En savoir plus"
+    
+            colDiv.appendChild(cardDiv);
+            cardDiv.appendChild(img);
+            cardDiv.appendChild(cardBodyDiv);
+            cardBodyDiv.appendChild(h5);
+            cardBodyDiv.appendChild(p);
+            cardBodyDiv.appendChild(a);
+    
+            imageBox.appendChild(colDiv);
+            numImage++;
+        });
+    
+        isLoad = true;
+
+
+
     }).catch(function(err){
         console.log(err);
     });
 
-    srcImages.forEach(src => {
-        let colDiv = document.createElement("div");
-        let cardDiv = document.createElement("div");
-        let img = document.createElement("img");
-        let cardBodyDiv = document.createElement("div");
-        let h5 = document.createElement("h5");
-        let p = document.createElement("p");
-        let a = document.createElement("a");
-
-        colDiv.className = "col col-sm-4";
-
-        cardDiv.className = "card";
-        cardDiv.style = "width: 18rem;";
-
-        img.src = src;
-        img.className = "card-img-top";
-
-        cardBodyDiv.className = "card-body";
-
-        h5.className = "card-title"
-        h5.textContent = "Titre " + numImage;
-
-        p.className = "card-text";
-        p.textContent = "Image " + numImage;
-
-        a.className = "btn btn-primary"
-        a.href = "#"
-        a.textContent = "En savoir plus"
-
-        colDiv.appendChild(cardDiv);
-        cardDiv.appendChild(img);
-        cardDiv.appendChild(cardBodyDiv);
-        cardBodyDiv.appendChild(h5);
-        cardBodyDiv.appendChild(p);
-        cardBodyDiv.appendChild(a);
-
-        imageBox.appendChild(colDiv);
-        numImage++;
-    });
-
-    isLoad = true;
+    
 }
     
 
