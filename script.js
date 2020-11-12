a80a0aa764db9a29443db26733664852a484f58e
alexandremessang <alexandre.messang@gmail.com>
2020-11-12 13:35:04 +0100

    set previous galery

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function loadImages() {
     let numImage = 1;
     
 
-    window.fetch("https://imgur.com/gallery/OCbnY").then(function(res){
+    window.fetch("https://api.imgur.com/3/gallery/hot/viral/0.json").then(function(res){
         return res.json();
     }).then(function(response){
         console.log(response);
