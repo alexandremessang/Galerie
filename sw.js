19e4ea1fd0ef7a82e637b1be321e004665e5cfa2
alexandremessang <alexandre.messang@gmail.com>
2020-11-12 14:17:18 +0100

    fix SW

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('message',event=>{
+    this.clients.matchAll().then(clients=>{
+        clients.forEach(client=>client.postMessage('Enchanté, je suis le service worker'));
+    });
+});
+
+Self.addEventListener('fetch',event=>{
+    event.RespondWith(newResponse('PWA!!!!'));
+});
+
+self.addEventListener('install',event=>{         
+    event.waitUntil(Promise.resolve('Install phase succeed'));
+});
+
+/*self.addEventListener('activate',function(event){         
+    event.waitUntil();
+});*/
+
+/**
+ * Suppresion de SW
+ * 
+ * if('serviceWorker'in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations=> {
+        for (let registration of registrations){
+            registrations.unregister();
+        }
+    }
+}
+*/
