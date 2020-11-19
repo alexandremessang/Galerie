5932e3a32fe526f44f2866e0c6790301b4ea54d3
alexandremessang <alexandre.messang@gmail.com>
2020-11-19 09:52:12 +0100

    fix api

--- script.js
+++ script.js
@@ -40,10 +40,12 @@ function loadImages() {
             cardDiv.className = "card";
             cardDiv.style = "width: 18rem;";
             
-            if (src.images !== 'undefined')
+            if (src.images !== 'undefined') {
+            console.log(src.images);
                 img.src = src.images[0].link;
-            else
+            } else {
                 img.src = src.link;
+            }
             img.className = "card-img-top";
     
             cardBodyDiv.className = "card-body";
